5cca1e30c7362a5fe2104eb6ab0e1a2e10836080
Trent Schafer <trent.schafer@gmail.com>
2014-07-01 19:04:59 +1000

    Upadte install file

--- install.sql
+++ install.sql
@@ -6,4 +6,10 @@ set define off
 @gapi_auth.pkb;
 @gapi_drive_file.pks;
 @gapi_drive_file.pkb;
+@gapi_drive_about.pks;
+@gapi_drive_about.pkb;
+@gapi_drive_change.pks;
+@gapi_drive_change.pkb;
+@gapi_cal_calendar.pks;
+@gapi_cal_calendar.pkb;
 grant execute on gapi_auth to apex_public_user;
