c799e29a21a92d7459d2d941b3b668ba75b86b31
Trent Schafer <trent.schafer@gmail.com>
2014-07-02 14:15:42 +1000

    Update install script

--- install.sql
+++ install.sql
@@ -1,15 +1,17 @@
 set define off
 
-@gapi_core.pks;
-@gapi_core.pkb;
+@gapi.pks;
+@gapi.pkb;
 @gapi_auth.pks;
 @gapi_auth.pkb;
+@gapi_cal.pks;
+@gapi_cal_calendar.pks;
+@gapi_cal_calendar.pkb;
+@gapi_cal_event.pks;
+@gapi_cal_event.pkb;
 @gapi_drive_file.pks;
 @gapi_drive_file.pkb;
 @gapi_drive_about.pks;
 @gapi_drive_about.pkb;
 @gapi_drive_change.pks;
 @gapi_drive_change.pkb;
-@gapi_cal_calendar.pks;
-@gapi_cal_calendar.pkb;
-grant execute on gapi_auth to apex_public_user;
